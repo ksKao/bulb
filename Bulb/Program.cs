f5c92f4b231927772901aa3095cc4de753afebb6
Kai Siang Kao <work@ks-kao.com>
2025-11-12 08:11:14 +0800

    Add simple file reader

--- Bulb/Program.cs
+++ Bulb/Program.cs
@@ -4,6 +4,21 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            if (args.Length == 0)
+                throw new ArgumentException("File path is reuiqred.");
+            
+            string filePath = args[0];
+
+            string fileContent = File.ReadAllText(filePath);
+            
+            Console.WriteLine(fileContent);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            Environment.Exit(1);
+        }
     }
 }
